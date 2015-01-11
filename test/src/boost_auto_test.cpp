8f405f18f0ac2d2b1a25e7a48a4f50d05a410a14
Povilas Balciunas <balciunas90@gmail.com>
2015-01-11 15:56:41 +0200

    Added filter_iterator tests.

--- test/src/boost_auto_test.cpp
+++ test/src/boost_auto_test.cpp
@@ -3,9 +3,13 @@
 #include <gmock/gmock.h>
 
 #include <boost/typeof/typeof.hpp>
+#include <boost/iterator/filter_iterator.hpp>
 
 
 using namespace ::testing;
+
+namespace boost
+{
 
 
 TEST(boost_auto, deduces_pair_type)
@@ -15,3 +19,31 @@ TEST(boost_auto, deduces_pair_type)
 	ASSERT_THAT(item.first, Eq("milk"));
 	ASSERT_THAT(item.second, Eq(1.12));
 }
+
+
+struct odd_number {
+	bool
+	operator()(int num)
+	{
+		return (num % 2) != 0;
+	}
+};
+
+TEST(boost_filter_iterator_create,
+	returns_iterator_pointing_to_first_element_satisfying_predicate)
+{
+	std::vector<int> numbers;
+	numbers.push_back(2);
+	numbers.push_back(4);
+	numbers.push_back(5);
+
+	odd_number predicate;
+	BOOST_AUTO(it, make_filter_iterator<odd_number>(numbers.begin(),
+		numbers.end())
+	);
+
+	ASSERT_THAT(*it, Eq(5));
+}
+
+
+} // namespace boost.
